9fafbe5c90b1bb9559ced168701e72cfee88002a
RaviNautiyal <ravinautiyal08@gmail.com>
2024-08-19 09:56:20 +0530

    Change in directory for Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 export default function logo({width = '100px'}) {
   return (
     <div>
-      <img src="/public/r_logo_4x.jpg" width=" 100px" className='border-2 border-sky-300 rounded-full'
+      <img src="/r_logo_4x.jpg" width=" 100px" className='border-2 border-sky-300 rounded-full'
       alt="Logo" />
     </div>
   )
